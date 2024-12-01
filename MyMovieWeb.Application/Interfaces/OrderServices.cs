776fb2fe34a678818b1f531573d80442d8b72b69
ThuatPhan <thuatphan761@gmail.com>
2024-12-01 18:42:22 +0700

    Add order api

--- MyMovieWeb.Application/Interfaces/OrderServices.cs
+++ MyMovieWeb.Application/Interfaces/OrderServices.cs
@@ -1,15 +1,10 @@
 ﻿using MyMovieWeb.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyMovieWeb.Application.Interfaces
 {
     public interface IOrderServices
     {
-        Task CreateOrder (Order order);
-
+        Task CreateOrder(Order order);
+        Task<Result<bool>> IsUserBoughtMovie(string userId, int movieId);
     }
 }
