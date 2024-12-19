3e53816bd3289cefe51bb46694c2e5a4d2aea245
ThuatPhan <thuatphan761@gmail.com>
2024-12-19 11:49:16 +0700

    Add api create payment intent

--- MyMovieWeb.Application/DTOs/Requests/PaymentRequestDTO.cs
+++ MyMovieWeb.Application/DTOs/Requests/PaymentRequestDTO.cs
@@ -2,10 +2,10 @@
 
 namespace MyMovieWeb.Application.DTOs.Requests
 {
-    public class CreatePaymentRequest
+    public class PaymentRequestDTO
     {
         [Range(19000, double.MaxValue, ErrorMessage = "Amount must be at least 19000 VND.")]
-        public decimal Amount { get; set; }
+        public long Amount { get; set; }
         public Dictionary<string, string> Metadata { get; set; }
     }
 }
---
+++ MyMovieWeb.Infrastructure/Migrations/20241219041827_AlterTableOrderss.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMovieWeb.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableOrderss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
