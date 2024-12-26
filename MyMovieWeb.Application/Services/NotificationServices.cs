7d62ab94537662f95538c3486543a2ab0aa3f630
ThuatPhan <thuatphan761@gmail.com>
2024-12-26 18:43:19 +0700

    Modify noti service

--- MyMovieWeb.Application/Services/NotificationServices.cs
+++ MyMovieWeb.Application/Services/NotificationServices.cs
@@ -37,19 +37,17 @@ namespace MyMovieWeb.Application.Services
         {
             if (!userIds.Any()) return;
 
-            IEnumerable<Task> addTasks = userIds
-                .Select(userId => _notificationRepo
-                    .AddAsync(
-                        new Notification
-                        {
-                            UserId = userId,
-                            Message = message,
-                            Url = url
-                        }
-                    )
+            foreach (string userId in userIds)
+            {
+                await _notificationRepo.AddAsync(
+                    new Notification
+                    {
+                        UserId = userId,
+                        Message = message,
+                        Url = url
+                    }
                 );
-
-            await Task.WhenAll(addTasks);
+            }
         }
 
         public async Task<Result<NotificationDTO>> MarkAsRead(int id)
